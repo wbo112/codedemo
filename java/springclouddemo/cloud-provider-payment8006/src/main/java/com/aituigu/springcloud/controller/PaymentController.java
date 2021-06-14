f72db0eec02d2660a2d7ceddf47fdec3f4d220c6
liai426 <liai426@163.com>
2021-06-14 21:48:15 +0800

    提交springcloud demo

---
+++ java/springclouddemo/cloud-provider-payment8006/src/main/java/com/aituigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.aituigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+
+    @GetMapping("/payment/consul")
+    public String getPayment() {
+       return "springcloud with consul :"+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+
+
+}
