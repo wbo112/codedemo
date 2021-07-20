4a82c98e28444f08ae59865df01da661f246f8ed
wbo112 <wbo112@126.com>
2021-07-20 22:17:48 +0800

    提交springcloud demo

---
+++ java/springclouddemo/create.sql
@@ -0,0 +1,5 @@
+create table test.payment(
+                             id int not null auto_increment comment 'id',
+                             serial varchar(200) default '',
+                             primary key (id)
+)
