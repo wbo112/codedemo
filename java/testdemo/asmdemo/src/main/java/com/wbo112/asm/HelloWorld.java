05d4c1487fcedf0f828a79459eb121f84265c750
wbo112 <wbo112@126.com>
2021-07-25 21:24:56 +0800

    提交asm代码

---
+++ java/testdemo/asmdemo/src/main/java/com/wbo112/asm/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.wbo112.asm;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) throws IOException {
+        ClassReader cr=new ClassReader("com.wbo112.asm.HelloWorld1");
+        ClassWriter cw=new ClassWriter(cr,ClassWriter.COMPUTE_FRAMES);
+        AddTimerAdapter addTimerAdapter=new AddTimerAdapter(cw);
+        cr.accept(addTimerAdapter, 0);
+        Files.createDirectories(Paths.get("asm"));
+        OutputStream outputStream = Files.newOutputStream(Paths.get("asm/helloworld1.class"));
+        outputStream.write(cw.toByteArray());
+        outputStream.close();
+
+    }
+
+
+}
