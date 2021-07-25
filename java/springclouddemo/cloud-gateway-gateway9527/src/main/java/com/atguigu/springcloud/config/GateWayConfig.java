1403e79e12fb97f866adcd142cf18f3c0d470c77
wbo112 <wbo112@126.com>
2021-07-25 21:28:41 +0800

    提交springcloud demo

---
+++ java/springclouddemo/cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/guonei")
+                        .uri("https://news.baidu.com/guonei"))
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .route("rewrite_route", r -> r.host("*.rewrite.org")
+                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
+                        .uri("http://httpbin.org"))
+                .route("hystrix_route", r -> r.host("*.hystrix.org")
+                        .filters(f -> f.hystrix(c -> c.setName("slowcmd")))
+                        .uri("http://httpbin.org"))
+                .route("hystrix_fallback_route", r -> r.host("*.hystrixfallback.org")
+                        .filters(f -> f.hystrix(c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback")))
+                        .uri("http://httpbin.org"))
+                .build();
+    }
+}
