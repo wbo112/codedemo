dbccc9b2d2349cc4ea90f7b4365429112a1cd5bb
wbo112 <wbo112@126.com>
2021-07-27 22:53:24 +0800

    提交asm代码

--- java/testdemo/asmdemo/src/main/java/com/wbo112/asm/HelloWorld.java
+++ java/testdemo/asmdemo/src/main/java/com/wbo112/asm/HelloWorld.java
@@ -1,8 +1,9 @@
 package com.wbo112.asm;
 
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.Method;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,14 +14,17 @@ public class HelloWorld {
 
 
     public static void main(String[] args) throws IOException {
-        ClassReader cr=new ClassReader("com.wbo112.asm.HelloWorld1");
-        ClassWriter cw=new ClassWriter(cr,ClassWriter.COMPUTE_FRAMES);
-        AddTimerAdapter addTimerAdapter=new AddTimerAdapter(cw);
+        ClassReader cr = new ClassReader("HelloWorld1");
+        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES);
+        AddTimerAdapter addTimerAdapter = new AddTimerAdapter(cw);
         cr.accept(addTimerAdapter, 0);
         Files.createDirectories(Paths.get("asm"));
         OutputStream outputStream = Files.newOutputStream(Paths.get("asm/helloworld1.class"));
         outputStream.write(cw.toByteArray());
         outputStream.close();
+
+        System.out.println(Type.getMethodType("(J)Ljava/lang/String;").getArgumentTypes()[0].getClassName());
+        System.out.println(Type.getMethodType("(J)Ljava/lang/String;").getReturnType().getClassName());
 
     }
 
